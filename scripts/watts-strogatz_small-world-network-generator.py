e2f8492c374f87fca8a9f05da618e7c5104e07f6
Tyllis Xu <LivelyCarpet87@gmail.com>
2023-07-18 01:45:49 -0500

    move exercise scripts

--- scripts/watts-strogatz_small-world-network-generator.py
+++ scripts/watts-strogatz_small-world-network-generator.py
@@ -2,16 +2,18 @@ import networkx as nx
 import random
 import matplotlib.pyplot as plt
 
+SIZE = 10
+
 edges = []
-for i in range(0,100):
-    new_edge = [i, (i+1)%100]
+for i in range(0,SIZE):
+    new_edge = [i, (i+1)%SIZE]
     edges.append(new_edge)
 
 new_edges = []
 for edge in edges:
     if random.randint(0,1) == 1:
-        while edge[1] == edge[0] or edge[1] == (edge[0] + 1)%100:
-            edge[1] = random.randint(0,99)
+        while edge[1] == edge[0] or edge[1] == (edge[0] + 1)%SIZE:
+            edge[1] = random.randint(0,SIZE-1)
     new_edges.append(edge)
 print(new_edges)
 
