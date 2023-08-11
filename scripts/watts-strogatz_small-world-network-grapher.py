44ae066c52a8979b3c08ed4b51260b2260d86791
Tyllis Xu <LivelyCarpet87@gmail.com>
2023-08-11 09:57:56 -0500

    Update watts-strogatz_small-world-network-grapher.py

--- scripts/watts-strogatz_small-world-network-grapher.py
+++ scripts/watts-strogatz_small-world-network-grapher.py
@@ -4,8 +4,19 @@ import matplotlib.pyplot as plt
 import helpers.metrics as m
 import pandas as pd
 from tqdm import tqdm
+import sqlite3
+import helpers.visualize as v
 
 SIZE = 1000
+
+#con = sqlite3.connect(":memory:")
+con = sqlite3.connect("./debug.db")
+
+"""
+
+cur = con.cursor()
+cur.execute("CREATE TABLE edgelist(source INT, destination INT, weight FLOAT)")
+con.commit()
 
 edges = []
 for j in range(1,21):
@@ -18,13 +29,17 @@ for edge in edges:
     if random.randint(1,10) == 1:
         while edge[1] == edge[0] or edge[1] == (edge[0] + 1)%SIZE:
             edge[1] = random.randint(0,SIZE-1)
-    new_edges.append(edge)
+    cur.execute("INSERT INTO edgelist VALUES(?, ?, ?)", edge)
+cur.execute("INSERT INTO edgelist SELECT destination, source, weight FROM edgelist")
 
-#G = nx.Graph()
-#G.add_edges_from(new_edges)
-#nx.draw(G)
-#plt.show()
+con.commit()
 
-df = pd.DataFrame(new_edges)
-print(m.calculate_geodesic_k(df,100, 10))
-print(m.calculate_geodesic_tau(df,100, 75))
+"""
+#m.generate_v_func()
+m.convert_to_g_sqlite(con)
+#m.calculate_geodesic_k_sqlite(con,1,5)
+#v.graph_information_neighborhood(con)
+#m.calculate_geodesic_k_sqlite_cross(con,SIZE)
+#v.graph_conditional_probability(con)
+
+con.commit()
